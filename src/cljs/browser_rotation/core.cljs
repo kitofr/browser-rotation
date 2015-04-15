1bed2e42da4eb4123b06e5efd2220c2f38694897
Kristoffer Roupé <kitofr@gmail.com>
2015-04-15 23:04:41 +0200

    WIP: Cant add users

--- src/cljs/browser_rotation/core.cljs
+++ src/cljs/browser_rotation/core.cljs
@@ -10,9 +10,66 @@
 ;; -------------------------
 ;; Views
 
+(defn k [pair]
+  (-> pair keys first))
+
+(defn value [pair]
+  (-> pair vals first))
+
+(defn browser-links [n]
+  (->> [{:chrome "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSx_ewOJFxPKntWJvSsEqSs1bOrbgV6OEzdG-D2lxIU1tJXIjGcRAjbvfdA" }
+        {:firefox "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRl842DwO1aFB0s2RjP2NOIatW34mTWmXpZeL8bfcvHR82cAz-yGGSuxh1c" }
+        {:safari "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShE2MlWD_XfWH8DnMlckF_e9Y4OougOzzDiL_9OMP_pGjtMfYAp4TJlOzg" }
+        {:ie "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSX1WBUmFYhJDHSA6qxeoFCN8h2zl3SLOcrXOdBmVc5Jaz3GcsAq4qeArHi" }
+        {:opera "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSc3W-bMSPcREOssHYIso-Wh8KGQylj4FAOUG_zGNIjrik04NdvRSUxlF4G" }]
+    cycle
+    (drop n)
+    (take 5)
+    vec))
+
+(defn browser-cycle [user i]
+  ^{:key user} 
+  [:tr 
+   [:td user]
+   (for [link (browser-links i)]
+     [:td 
+      ^{:key (k link)}
+      [:img {:id i :src (value link) :width 100 :height 100 }]])])
+
+(defn add-user [users text]
+  (swap! users conj text))
+
+(defn atom-input [users]
+   [:input {:type "text" 
+            :placeholder "Developer name"
+            :on-key-down #(case (.-which %)
+                           13 (add-user users (-> % .-target .-value))) }])
+
+(defn browser-rotation [users]
+  (let [days ["Monday" "Tuesday" "Wednesday" "Thursday" "Friday"]]
+       [users (atom ["Kristoffer" 
+                     "Lennart"
+                     "Dennis"
+                     "Victoria"
+                     "Martin"
+                     "Mikael"
+                     "Kalle"])]
+    [:div
+    [atom-input @users]
+    [:table
+     [:tr 
+      [:th]
+      (for [day days]
+        ^{:key day} [:th day])
+        (map-indexed 
+          (fn [i user]
+            [browser-cycle user i]) users)]]]))
+      
 (defn home-page []
   [:div [:h2 "Welcome to browser-rotation"]
-   [:div [:a {:href "#/about"} "go to about page"]]])
+   [:div [:a {:href "#/about"} "go to about page"]]
+   [:h3 "Browser schedule"]
+   [:div [browser-rotation]]])
 
 (defn about-page []
   [:div [:h2 "About browser-rotation"]
